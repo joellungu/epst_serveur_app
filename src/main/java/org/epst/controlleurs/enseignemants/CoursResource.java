129a0ee024fbbd5a0e78eef98f03ad3b6f987022
joellungu <lungujoel138@gmail.com>
2025-11-02 21:33:06 +0100

    correction erreur route cours

--- src/main/java/org/epst/controlleurs/enseignemants/CoursResource.java
+++ src/main/java/org/epst/controlleurs/enseignemants/CoursResource.java
@@ -8,7 +8,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/cours")
+@Path("/courss")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CoursResource {
